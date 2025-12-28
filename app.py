3f33992081576b558ffcc3839a37eead96a7327a
yvener41 <dutervilyvener41@gmail.com>
2025-12-28 00:46:42 -0500

    Add Railway deployment configuration

--- app.py
+++ app.py
@@ -237,4 +237,6 @@ def add_topping():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
